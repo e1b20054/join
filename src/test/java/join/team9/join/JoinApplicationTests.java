60d645db901bf47aa57a4c83a085c70ab7aa4476
e1b20054 <e1b20054@oit.ac.jp>
2022-10-11 14:22:00 +0900

    リポジトリの初期化

---
+++ src/test/java/join/team9/join/JoinApplicationTests.java
@@ -0,0 +1,13 @@
+package join.team9.join;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

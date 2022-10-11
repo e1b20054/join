c15ba75b941b4a2184df93c02218bb1a2794b26e
e1b20023 <e1b20023@oit.ac.jp>
2022-10-11 15:03:08 +0900

    T8の実施

--- src/main/java/join/team9/join/security/Sample3AuthConfiguration.java
+++ src/main/java/join/team9/join/security/Sample3AuthConfiguration.java
@@ -16,13 +16,9 @@ public class Sample3AuthConfiguration {
 
   @Bean
   public InMemoryUserDetailsManager userDetailsService() {
-    // このクラスの下部にあるPasswordEncoderを利用して，ユーザのビルダ（ログインするユーザを作成するオブジェクト）を作成する
+
     UserBuilder users = User.builder();
 
-    // UserBuilder usersにユーザ名，パスワード，ロールを指定してbuildする
-    // このときパスワードはBCryptでハッシュ化されている．
-    // ハッシュ化されたパスワードを得るには，この授業のbashターミナルで下記のように末尾にユーザ名とパスワードを指定すると良い(要VPN)
-    // $ sshrun htpasswd -nbBC 10 user1 p@ss
     UserDetails user1 = users
         .username("user1")
         .password("$2y$10$9dWh5RpU05HhUJNdBFFQduiBvdRdYNddO2ugD8nY2XcABCKuw7ZZ.")
@@ -38,19 +34,16 @@ public class Sample3AuthConfiguration {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    // Spring Securityのフォームを利用してログインを行う（自前でログインフォームを用意することも可能）
+
     http.formLogin();
 
-    // http://localhost:8000/sample3 で始まるURLへのアクセスはログインが必要
-    // mvcMatchers().authenticated()がmvcMatchersに指定されたアクセス先に認証処理が必要であることを示す
-    // authenticated()の代わりにpermitAll()と書くと認証不要となる
     http.authorizeHttpRequests()
         .mvcMatchers("/sample3/**").authenticated();
 
     // http.authorizeHttpRequests()
     // .mvcMatchers("/sample3/**").permitAll();
 
-    http.logout().logoutSuccessUrl("/"); // ログアウト時は "http://localhost:8000/" に戻る
+    http.logout().logoutSuccessUrl("/");
     return http.build();
   }
 
